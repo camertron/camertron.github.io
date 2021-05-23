78b97c838267c2ac7a67098802fb15f1cbf11c80
Cameron Dutro <camertron@gmail.com>
2021-05-22 19:19:10 -0700

    Almost there

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,7 +6,7 @@ BRIDGETOWN_ENV=production yarn build
 git checkout deploy
 git ls-files | xargs rm -f
 git ls-tree --name-only -d -r HEAD | sort -r | xargs rmdir
-git checkout .gitignore .nojekyll
+git checkout .gitignore .nojekyll CNAME
 cp -R output/ ./
 exit 0
 git add -A
