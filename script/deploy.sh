4b830ab2ad5f26ed5303a1b2627b3d326d159f82
Cameron Dutro <camertron@gmail.com>
2021-05-22 17:37:48 -0700

    Print out commands; change back to main branch

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-set -e
+set -ex
 
 BRIDGETOWN_ENV=production yarn build
 git checkout deploy
@@ -8,3 +8,4 @@ cp -R output/ ./
 git add -A
 git commit -m "Update build"
 git push origin deploy
+git checkout main
