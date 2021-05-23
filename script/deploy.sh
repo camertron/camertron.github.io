282a2be45244093a16437ef4b5e31224113079c7
Cameron Dutro <camertron@gmail.com>
2021-05-22 19:17:53 -0700

    Remove all files?

--- script/deploy.sh
+++ script/deploy.sh
@@ -4,7 +4,11 @@ set -ex
 
 BRIDGETOWN_ENV=production yarn build
 git checkout deploy
+git ls-files | xargs rm -f
+git ls-tree --name-only -d -r HEAD | sort -r | xargs rmdir
+git checkout .gitignore .nojekyll
 cp -R output/ ./
+exit 0
 git add -A
 git commit -m "Update build"
 git push origin HEAD
